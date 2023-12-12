5981501d1c309951dda4d84f513dcce18705daa6
Royce551 <poohwie2005@gmail.com>
2023-12-11 21:18:17 -0600

    Implement reader options

--- Kotomi/Kotomi/ViewModels/ReaderViewModel.cs
+++ Kotomi/Kotomi/ViewModels/ReaderViewModel.cs
@@ -1,4 +1,7 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Data;
+using Avalonia.LogicalTree;
+using Avalonia.Media;
 using CommunityToolkit.Mvvm.ComponentModel;
 using Kotomi.Models.Series;
 using System;
@@ -31,28 +34,106 @@ namespace Kotomi.ViewModels
         [NotifyPropertyChangedFor(nameof(CurrentPage))]
         private int page = 1;
 
-        public Control CurrentPage => new Image()
+        // TODO: yikes! UI code in the VM layer? not ideal at all
+        public Control? CurrentPage
         {
-            Source = CurrentChapter.GetPage(page)
-        };
+            get
+            {
+                if (ReadingModeSingle) return new Image() { Source = CurrentChapter.GetPage(page) };
+                if (ReadingModeTwo)
+                {
+                    var grid = new Grid() { ColumnDefinitions = new("*,*") };
+                    var leftImage = new Image()
+                    {
+                        Source = CurrentChapter.GetPage(page),
+                    };
+                    if (ReadingDirectionLeftToRight) Grid.SetColumn(leftImage, 0);
+                    else Grid.SetColumn(leftImage, 1);
+                    grid.Children.Add(leftImage);
+
+                    var rightImage = new Image()
+                    {
+                        Source = CurrentChapter.GetPage(page + 1),
+                    };
+                    if (ReadingDirectionLeftToRight) Grid.SetColumn(rightImage, 1);
+                    else Grid.SetColumn(rightImage, 0);
+                    grid.Children.Add(rightImage);
+                    return grid;
+                }
+                if (ReadingModeLong)
+                {
+                    var scrollViewer = new ScrollViewer();
+                    var stackPanel = new StackPanel();
+                    for (int i = 1; i < CurrentChapter.TotalPages; i++)
+                    {
+                        var imageSource = CurrentChapter.GetPage(i);
+                        stackPanel.Children.Add(new Image() { Source = imageSource, MaxHeight = imageSource.Size.Height, Margin = new(0, 5) });
+                    }
+                    scrollViewer.Content = stackPanel;
+                    return scrollViewer;
+                }
+                else return null;
+            }
+        }
 
         public IChapter CurrentChapter => series.Chapters[SelectedChapterIndex];
 
         public void SwitchToLibraryView() => MainView.NavigateTo(new LibraryViewModel());
 
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(CurrentPage))]
+        private bool readingModeSingle = true;
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(CurrentPage))]
+        private bool readingModeTwo;
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(CurrentPage))]
+        private bool readingModeLong;
+
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(CurrentPage))]
+        private bool readingDirectionLeftToRight;
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(CurrentPage))]
+        private bool readingDirectionRightToLeft = true;
+        
         public void PageLeft()
         {
-            Page--;
+            var pagesToTurn = ReadingModeSingle ? 1 : 2;
+
+            if (ReadingDirectionLeftToRight)
+            {
+                
+                if (Page - pagesToTurn >= 1)
+                    Page -= pagesToTurn;
+            }
+            else
+            {
+                if (Page + pagesToTurn <= CurrentChapter.TotalPages)
+                    Page += pagesToTurn;
+            }
         }
 
         public void PageRight()
         {
-            Page++;
+            var pagesToTurn = ReadingModeSingle ? 1 : 2;
+
+            if (ReadingDirectionLeftToRight)
+            {
+
+                if (Page + pagesToTurn <= CurrentChapter.TotalPages)
+                    Page += pagesToTurn;
+            }
+            else
+            {
+                if (Page - pagesToTurn >= 1)
+                    Page -= pagesToTurn;
+            }
         }
 
         [ObservableProperty]
         private bool isMenuBarShown = true;
 
-        public void ShowMenuBar() => IsMenuBarShown = true;
+        public void ShowHideMenuBar() => IsMenuBarShown = !IsMenuBarShown;
     }
 }
